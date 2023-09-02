dafd835f29781fd65d4b1173b83b509d7100de66
rahulsweb <rahulsonawanen@gmail.com>
2023-09-02 21:30:32 +0530

    changed added

---
+++ src/api/callApi.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+let options = {
+    method: 'GET',
+    headers: {
+      'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com',
+      'x-rapidapi-key': '4ac5e3352fmshe6ac515ca3b8ccap1f0045jsnf0a504a87bbe'
+    }
+
+  };
+export async function callGetApi(url,param){
+    options.url= url;
+    options.params= param;
+    return await axios.request(options).then(function (response) {
+    return response.data.data;
+   }).catch(function (error) {
+    alert(error);
+    return false;
+   });
+
+   }
